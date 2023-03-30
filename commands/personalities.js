ed1777fb90d7f267b499a90f4c500350aac6c925
Jordan Rogers <jrdnrgrs@gmail.com>
2023-03-30 09:40:35 -0500

    use discord embeds for personalities output

--- commands/personalities.js
+++ commands/personalities.js
@@ -1,5 +1,5 @@
 // Requre the necessary discord.js classes
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
 // Initialize .env file
 require('dotenv').config({ path: '/.env'});
 
@@ -15,14 +15,22 @@ module.exports = {
             await interaction.reply(process.env.DISABLED_MSG);
             return;
         }
-        // Create message variable
-		persMsg = process.env.PERSONALITY_MSG + "\n";
-		// Add personality names to variable
-		for (let i = 0; i < state.personalities.length; i++) {
-			let thisPersonality = state.personalities[i];
-			persMsg += "- " + thisPersonality.name + "\n"
-		}
+        // Create an embed object
+        let persEmbed = new EmbedBuilder()
+            .setColor(0x0099FF) // set the color of the embed
+            .setTitle(process.env.PERSONALITY_MSG) // set the title of the embed
+            .setDescription('Here are the personalities and their prompts'); // set the description of the embed
+        
+        // Add personality names and prompts to fields
+        for (let i = 0; i < state.personalities.length; i++) {
+            let thisPersonality = state.personalities[i];
+            // Find the prompt from the request array
+            let prompt = thisPersonality.request.find(item => item.role === 'system').content;
+            // Truncate the prompt to 1024 characters if it's longer than that
+            let truncatedPrompt = prompt.substring(0, 1024);
+            persEmbed.addFields({ name: thisPersonality.name, value: truncatedPrompt });
+        }
 		// Send variable
-		interaction.reply(persMsg);
+        interaction.reply({ embeds: [persEmbed] });
     },
 };
