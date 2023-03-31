214d8b178e15e76283e21fcf2956606f6d56a93a
Kevin8675 <kevinkraze246@gmail.com>
2023-03-31 11:09:55 -0400

    Remove embed description

--- commands/personalities.js
+++ commands/personalities.js
@@ -7,7 +7,7 @@ module.exports = {
     data: new SlashCommandBuilder()
         // Command details
         .setName('personalities')
-        .setDescription('List the name of all personalities.'),
+        .setDescription('List the name of all personalities and their prompts.'),
     async execute(interaction, state) {
         // Commands to execute
         // Check admin/pause state
@@ -19,8 +19,6 @@ module.exports = {
         let persEmbed = new EmbedBuilder()
             .setColor(0x0099FF) // set the color of the embed
             .setTitle(process.env.PERSONALITY_MSG) // set the title of the embed
-            .setDescription('Here are the personalities and their prompts'); // set the description of the embed
-        
         // Add personality names and prompts to fields
         for (let i = 0; i < state.personalities.length; i++) {
             let thisPersonality = state.personalities[i];
